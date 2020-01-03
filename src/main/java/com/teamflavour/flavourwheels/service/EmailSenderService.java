496fc64ed699e1efb91f80603999829f9be15faa
Jason Bakker <jasonbakker084@gmail.com>
2020-01-03 17:56:58 +0100

    change emailSenderService

--- src/main/java/com/teamflavour/flavourwheels/service/EmailSenderService.java
+++ src/main/java/com/teamflavour/flavourwheels/service/EmailSenderService.java
@@ -2,20 +2,16 @@ package com.teamflavour.flavourwheels.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.Properties;
-
-@Service("emailSenderService")
+@Service
 public class EmailSenderService {
 
     @Autowired
-    private JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
 
     @Autowired
     public EmailSenderService(JavaMailSender javaMailSender) {
