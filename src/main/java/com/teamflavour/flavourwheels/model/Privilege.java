f17d99cd5995a5344201387b6bdaabc1b100511c
Jason Bakker <jasonbakker084@gmail.com>
2020-01-04 15:46:05 +0100

    add table privilege

--- src/main/java/com/teamflavour/flavourwheels/model/Privilege.java
+++ src/main/java/com/teamflavour/flavourwheels/model/Privilege.java
@@ -1,13 +1,10 @@
 package com.teamflavour.flavourwheels.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.Collection;
 
 @Entity
+@Table(name = "Privilege")
 public class Privilege {
 
     @Id
