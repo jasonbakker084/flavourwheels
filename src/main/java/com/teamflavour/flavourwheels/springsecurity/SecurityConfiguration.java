e86ef41f1d645b03422aa30a301e76d62f2f4f2e
Jason Bakker <jasonbakker084@gmail.com>
2020-01-09 14:23:06 +0100

    going back to wheel.js

--- src/main/java/com/teamflavour/flavourwheels/springsecurity/SecurityConfiguration.java
+++ src/main/java/com/teamflavour/flavourwheels/springsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.teamflavour.flavourwheels.springsecurity;
 
 
 import com.teamflavour.flavourwheels.service.UserServiceImpl;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
